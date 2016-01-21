299ea5c389162aeabd641402c4d2641b22e553a9
Ce Shang <shangce919@hotmail.com>
2016-01-21 00:58:29 -0600

    project-grid.js

---
+++ js/project-grid.js
@@ -0,0 +1,31 @@
+/**
+ * Created by ceshang on 1/20/2016.
+ */
+$(".isotope-item").click(function(){
+    if($(this).hasClass("card-small")){
+        $(".isotope-item").removeClass("active");
+        $(this).toggleClass("active");
+
+    }
+    else{
+        $("#project-detail").animate({
+            height:"300px",
+            overflow:"auto"
+        },1500);
+        $(".isotope-item").removeClass("card");
+        $(".isotope-item").addClass("card-small");
+        $(this).toggleClass("active");
+    }
+
+});
+
+$("#project-detail").find(".fa-chevron-circle-up").click(function(){
+    $("#project-detail").animate({
+       height:"0",
+       overflow:"hidden"
+    },500);
+    $(".isotope-item").removeClass("card-small");
+    $(".isotope-item").removeClass("active");
+    $(".isotope-item").addClass("card");
+});
+
